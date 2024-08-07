a43434c369b1a970f8d99f34ce5d896445b28343
egelipta <egelipta@gmail.com>
2024-08-07 10:54:56 +0700

    feat(threejs): IMP 'endpoint-rack-server'

--- api/extends/rack_server.py
+++ api/extends/rack_server.py
@@ -15,6 +15,26 @@ from tortoise.queryset import F
 
 router = APIRouter(prefix='/rack_server')
 
+
+@router.post("", summary="Rack Server Add",
+# dependencies=[Security(check_permissions, scopes=["tugas_add"])]
+)
+async def rack_server_add(post: rack_server.CreateRackServer):
+    """
+    Tugas Add
+    :param post: CreateTugas
+    :return:
+    """
+    # Filter Tugass
+    # get_name_tugas = await Tugas.get_or_none(name_tugas=post.name_tugas)
+    # if get_name_tugas:
+    #     return fail(msg=f"Tugas {post.name_tugas} sudah ada!")
+
+    create_rack_server = await RackServer.create(**post.dict())
+    if not create_rack_server:
+        return fail(msg=f"Failed to create Tugas {post.name}!")
+    return success(msg=f"{create_rack_server.name} berhasil disimpan")
+
 @router.get("/data-rack-server", summary="Data Rack Server")
 async def data_rack_server():
     data = await RackServer.all()
--- schemas/rack_server.py
+++ schemas/rack_server.py
@@ -2,7 +2,7 @@
 """
 @Time : 2023/12/27 11:59 PM
 @Author: elge
-@Des: Gedung Schema
+@Des: RackServer Schema
 """
 from datetime import datetime
 from pydantic import Field, BaseModel, validator
@@ -10,7 +10,7 @@ from typing import Optional, List
 from schemas.base import BaseResp, ResAntTable
 
 
-class CreateGedung(BaseModel):
+class CreateRackServer(BaseModel):
     name: str = Field(min_length=1, max_length=255)
     width: int
     height: int
@@ -19,17 +19,17 @@ class CreateGedung(BaseModel):
     y: int
     z: int
     
-class UpdateGedung(CreateGedung):
+class UpdateRackServer(CreateRackServer):
     id: int
 
-class GedungItem(UpdateGedung):
+class RackServerItem(UpdateRackServer):
     key: int
     id: int
     create_time: datetime
     update_time: datetime
 
-class GedungDelete(BaseModel):
+class RackServerDelete(BaseModel):
     id: int
 
-class GedungListData(ResAntTable):
-    data: List[GedungItem]
+class RackServerListData(ResAntTable):
+    data: List[RackServerItem]
