bec28aaa809c8dae1e1b7e50e1446779ac259bcb
egelipta <egelipta@gmail.com>
2024-12-01 15:12:25 +0700

    feat(endpoint): add 'home-assistant/create-and-get'

---
+++ api/extends/home_assistant.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+"""
+@Time : 2024/11/28 11:59 PM
+@Author: elge
+@Des: HomeAssistant Extends
+"""
+
+import datetime
+from typing import Any, Dict, List, Optional
+from core.Response import success, fail, res_antd
+from models.home_assistant import HomeAssistant
+from schemas import home_assistant
+from schemas.home_assistant import CreateHomeAssistant, UpdateHomeAssistant
+from fastapi import HTTPException, Request, Query, APIRouter
+from tortoise.queryset import F
+import json
+
+router = APIRouter(prefix='/homeassistant')
+
+@router.get("/get-data", summary="Get All Data")
+async def get_all_data():
+    data = await HomeAssistant.all().order_by('-create_time')
+    return data
+
+@router.post("/add-data", summary="Add Data")
+async def home_assistant_add(post: CreateHomeAssistant):
+    """
+    Tugas Add
+    :param post: Create Workflow Editor
+    :return:
+    """
+    create_action = await HomeAssistant.create(**post.dict())
+    
+    if not create_action:
+        return fail(msg=f"Failed {post.id}!")
+    
+    return success(msg=f"{create_action.id} Berhasil ditambahkan")
---
+++ schemas/home_assistant.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+"""
+@Time : 2024/11/28 11:59 PM
+@Author: elge
+@Des: HomeAssistant Schema
+"""
+from datetime import datetime
+from pydantic import Field, BaseModel, validator
+from typing import Optional, List, Dict
+from schemas.base import BaseResp, ResAntTable
+
+
+class CreateHomeAssistant(BaseModel):
+    datachart: Dict = Field(default_factory=dict)
+    
+class UpdateHomeAssistant(CreateHomeAssistant):
+    id: int
+
+class HomeAssistantItem(UpdateHomeAssistant):
+    key: int
+    id: int
+    create_time: datetime
+    update_time: datetime
+
+class HomeAssistantDelete(BaseModel):
+    id: int
+
+class HomeAssistantListData(ResAntTable):
+    data: List[HomeAssistantItem]
